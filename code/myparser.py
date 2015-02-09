6f4cef76b170a7e10a6fb92dd1e09e6f7398b84c
QuentinY <quentin.yoshida@outlook.com>
2015-02-09 15:59:44 +0000

    Update myparser.py

--- code/myparser.py
+++ code/myparser.py
@@ -8,4 +8,5 @@ Feel free to modify and improve directly on this file.
 def parse(sentence):
 	sentence = sentence.replace('|','') # Remove conflicts with VW format
 	sentence = sentence.replace(':','&') # Remove conflicts with VW format
+	sentence = sentence.lower() # Convert all to lower-case
 	return sentence
